a9048b3e51c82b294829cda1bf41fc0f0f4dc091
ronnymikalsen <ronny.mikalsen@nb.no>
2017-06-20 15:25:45 +0200

    travisci

--- integration/protractor.config.js
+++ integration/protractor.config.js
@@ -7,9 +7,20 @@ const config = {
     'browserName': 'chrome'
   },
   baseUrl: 'http://localhost:8080/',
-  framework: 'jasmine',
-  sauceUser: 'ronnymikalsen',
-  sauceKey: 'fa5b212d-466a-4e29-b593-957164bfc10b'
+  framework: 'jasmine'
 };
+
+if (process.env['TRAVIS']) {
+  config.sauceUser = 'ronnymikalsen',
+  config.sauceKey = 'fa5b212d-466a-4e29-b593-957164bfc10b'
+  config.capabilities = {
+    'browserName': 'chrome',
+    'version': 'latest',
+    'chromedriverVersion': '2.28',
+    'tunnel-identifier': process.env['TRAVIS_JOB_ID'],
+    'build': process.env['TRAVIS_JOB_ID'],
+    'name': 'Mime E2E Tests',
+  };
+}
 
 exports.config = config;
