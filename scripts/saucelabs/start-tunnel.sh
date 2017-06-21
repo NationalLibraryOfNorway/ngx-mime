092ed56d2f3323c2f8248763260241772d43866a
ronnymikalsen <ronny.mikalsen@nb.no>
2017-06-21 10:29:55 +0200

    showing args

--- scripts/saucelabs/start-tunnel.sh
+++ scripts/saucelabs/start-tunnel.sh
@@ -38,4 +38,6 @@ fi
 
 echo "Starting Sauce Connect in the background, logging into: ${TUNNEL_LOG}"
 
+echo "args: ${ARGS}"
+
 sauce-connect/bin/sc -u ${SAUCE_USERNAME} -k ${SAUCE_ACCESS_KEY} ${ARGS} 2>&1 >> ${TUNNEL_LOG} &
