be6bf62f79baccdf4c1ced00bd03caa9403d01b0
ronnymikalsen <ronny.mikalsen@nb.no>
2017-06-22 12:45:26 +0200

    build(*): fixed comment
    Fixed comment

--- scripts/ci/after-success.sh
+++ scripts/ci/after-success.sh
@@ -4,7 +4,7 @@
 # Go to the project root directory
 cd $(dirname $0)/../..
 
-# If not running as a PR, wait for all other travis modes to finish.
+# If not running as a PR
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   echo "Travis tests passed on 'master'."
   npm run semantic-release
