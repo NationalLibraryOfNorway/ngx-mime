c51230a7b39c37e8f140680fd54a4d4238100c37
ronnymikalsen <ronny.mikalsen@nb.no>
2017-06-28 14:25:51 +0200

    test(mountebank): changed port
    Changed port on mountebank. See https://support.saucelabs.com/hc/en-us/articles/225106887-Safari-and-Internet-Explorer-Won-t-Load-Website-When-Using-Sauce-Connect-on-Localhost

--- integration/e2e/step-definitions/viewer-page.steps.ts
+++ integration/e2e/step-definitions/viewer-page.steps.ts
@@ -3,7 +3,7 @@ import { expect } from '../helpers/chai-imports'
 import { ViewerPage } from '../pages/viewer.po';
 
 defineSupportCode(function ({ Given }) {
-  const aLtrBook = 'http://localhost:4545/catalog/v1/iiif/a-ltr-book/manifest';
+  const aLtrBook = 'http://localhost:4040/catalog/v1/iiif/a-ltr-book/manifest';
   const viewerPage = new ViewerPage();
 
   Given(/^I am opening a book which are written from left to right$/, async () => {
