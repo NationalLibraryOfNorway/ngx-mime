2dca058dbe4a91109c9f5f0e3e8f77dd22f973cc
“raymondkarstensen” <raymond.karstensen@nb.no>
2017-07-13 12:30:46 +0200

    fix: Unsubscribe subscriptions

--- src/lib/src/viewer/viewer.component.ts
+++ src/lib/src/viewer/viewer.component.ts
@@ -1,17 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { IiifService } from '../core/iiif-service/iiif-service';
 import { Manifest } from '../core/models/manifest';
 import { Options } from '../core/models/options';
 import { ViewerBuilder } from '../core/builders/viewer.builder';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'mime-viewer',
   templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.scss']
 })
-export class ViewerComponent implements OnInit {
+export class ViewerComponent implements OnInit, OnDestroy {
   @Input() url: string;
   @Input() options: Options;
+  private subscriptions: Array<Subscription> = [];
 
   constructor(private iiifService: IiifService) { }
 
@@ -21,13 +23,21 @@ export class ViewerComponent implements OnInit {
     }
 
     if (this.url) {
-      this.iiifService.getManifest(this.url)
-        .subscribe((manifest: Manifest) => {
-          new ViewerBuilder()
-            .withOptions(this.options)
-            .withTiles(manifest.tileSource)
-            .create();
-        });
+      this.subscriptions.push(
+        this.iiifService.getManifest(this.url)
+          .subscribe((manifest: Manifest) => {
+            new ViewerBuilder()
+              .withOptions(this.options)
+              .withTiles(manifest.tileSource)
+              .create();
+          })
+      );
     }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
   }
 }
