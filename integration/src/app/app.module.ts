55335a07f0eeb3ed36bb2bd84664e072606597b1
“raymondkarstensen” <raymond.karstensen@nb.no>
2017-07-13 13:15:08 +0200

    fix: Don't need local file of protractor.conf fix: Needed to add CUSTOM_ELEMENTS_SCHEMA to allow uri and options attributes on mime-viewer tag

--- integration/src/app/app.module.ts
+++ integration/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MimeModule } from '@nationallibraryofnorway/ngx-mime';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent
   ],
