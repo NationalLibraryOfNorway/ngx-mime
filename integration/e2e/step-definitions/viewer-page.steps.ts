4e4ea8f18d587f837bf95f7670e0112e8435a055
“raymondkarstensen” <raymond.karstensen@nb.no>
2017-07-13 12:37:03 +0200

    fix: Don't use static methods

--- integration/e2e/step-definitions/viewer-page.steps.ts
+++ integration/e2e/step-definitions/viewer-page.steps.ts
@@ -3,9 +3,10 @@ import { expect } from '../helpers/chai-imports';
 import { ViewerPage } from '../pages/viewer.po';
 
 defineSupportCode(function ({ Given }) {
+  const page = new ViewerPage();
 
   Given(/^I am opening a default book$/, async () => {
-    await ViewerPage.open();
-    await expect(ViewerPage.getOpenSeadragon()).exist;
+    await page.open();
+    await expect(page.getOpenSeadragon()).exist;
   });
 });
