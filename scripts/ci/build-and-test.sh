de4be182e9e0f19ad21b087687ee1bf58638690c
Ronny Mikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2017-07-17 09:42:46 +0200

    ci: npm script for coverage (#31)
    * - Import OpenSeadragon
    - Created IiifService
    - Trying to find the best way to use OpenSeadragon with options
    
    * - Import OpenSeadragon
    - Created IiifService
    - Trying to find the best way to use OpenSeadragon with options
    
    * - Simple css refactoring
    
    * - model defaults
    
    * - Renaming and moving files to correct folder
    
    * - More builders
    - Renaming
    - Tests
    - Demo app sends options and a default id as inputs
    
    * - Renaming error
    
    * - Change input from id to url
    - removing unnecesseraly UrlBuilder
    
    * - BuilderUtils for extracting annotation-fields. Removing duplicate hardcoded values
    
    * - Merge
    
    * - Missing @angular/cdk in systemjs.config
    - Retry on customHttp in demo app
    
    * feat: Local config for e2e
    feat: Added OpenSeadragon to e2e app
    feat: Simple e2e for testing that the viewer opens correctly
    fix: Removing unused tests
    fix: Removing toolbar
    fix: Removing rulesDirectory for codelyzer in tslint as this produces error in lint
    
    * fix: Removing unnecesseray karma test since the toolbar has been removed
    
    * fix: Don't need this in scripts
    
    * fix: Lint didn't search in subdirectories
    
    * fix: Fixed lint errors
    
    * fix: Unsubscribe subscriptions
    
    * fix: Don't use static methods
    
    * fix: Missing Then in Scenario
    
    * fix: No need for CustomHttp
    
    * fix: Use test url
    
    * fix: Don't need local file of protractor.conf
    fix: Needed to add CUSTOM_ELEMENTS_SCHEMA to allow uri and options attributes on mime-viewer tag
    
    * BREAKING CHANGES: Changed "url" paramater to "manifestUri"
    fix: Didn't need CUSTOM_ELEMENTS_SCHEMA. Problem was ngx-mime in node_modules that didn't update correctly
    
    * fix: Wrong path to mime-viewer
    
    * ci: npm script for coverage
    
    Coverage shouldn't be the default with running npm test since it increases the time it takes to run unit tests.
    
    * ci: coverage instead of test:once
    
    No need to test it twice
    
    * ci: test
    
    * ci: test outside sauce labs
    
    * ci: test

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -12,11 +12,13 @@ cd $(dirname $0)/../..
 # Include sources.
 source ./scripts/ci/sources/tunnel.sh
 
+npm run lint
+npm run test:once
+npm run coverage
+
 start_tunnel
 wait_for_tunnel
 
-npm run lint
-npm run test:once
 npm run integration
 
 teardown_tunnel
