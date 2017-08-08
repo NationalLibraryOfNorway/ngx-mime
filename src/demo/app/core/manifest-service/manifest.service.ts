2b27031c4de8ccf728e49f0e2c1da6ae7b52a432
Ronny Mikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2017-08-08 08:12:03 +0200

    fix: relative manifest paths
    Relative manifest paths
    Angular cli upgrade

--- src/demo/app/core/manifest-service/manifest.service.ts
+++ src/demo/app/core/manifest-service/manifest.service.ts
@@ -8,31 +8,31 @@ export class ManifestService {
   static readonly manifests: ManifestMenuItem[] = [
     {
       label: 'LTR',
-      uri: 'http://localhost:3000/assets/fixtures/presentation/2/simple-ltr-manifest.json'
+      uri: 'assets/fixtures/presentation/2/simple-ltr-manifest.json'
     },
     {
       label: 'RTL',
-      uri: 'http://localhost:3000/assets/fixtures/presentation/2/simple-rtl-manifest.json'
+      uri: 'assets/fixtures/presentation/2/simple-rtl-manifest.json'
     },
     {
       label: 'Minimum',
-      uri: 'http://localhost:3000/assets/fixtures/presentation/2/minimum-manifest.json'
+      uri: 'assets/fixtures/presentation/2/minimum-manifest.json'
     },
     {
       label: 'Downloadable',
-      uri: 'http://localhost:3000/assets/fixtures/presentation/2/downloadble-manifest.json'
+      uri: 'assets/fixtures/presentation/2/downloadble-manifest.json'
     },
     {
       label: 'Searchable',
-      uri: 'http://localhost:3000/assets/fixtures/presentation/2/searchable-manifest.json'
+      uri: 'assets/fixtures/presentation/2/searchable-manifest.json'
     },
     {
       label: 'License',
-      uri: 'http://localhost:3000/assets/fixtures/presentation/2/license-manifest.json'
+      uri: 'assets/fixtures/presentation/2/license-manifest.json'
     },
     {
       label: 'Attribution',
-      uri: 'http://localhost:3000/assets/fixtures/presentation/2/attribution-manifest.json'
+      uri: 'assets/fixtures/presentation/2/attribution-manifest.json'
     }
   ];
 
