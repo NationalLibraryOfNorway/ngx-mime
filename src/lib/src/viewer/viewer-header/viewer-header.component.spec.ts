9b8ff9013eaabe4286d934b0a9dfccebe497e5ac
ronnymikalsen <ronny.mikalsen@nb.no>
2017-08-18 13:49:41 +0200

    test: testing  open contents

--- src/lib/src/viewer/viewer-header/viewer-header.component.spec.ts
+++ src/lib/src/viewer/viewer-header/viewer-header.component.spec.ts
@@ -1,27 +1,39 @@
-import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule, ObservableMedia } from '@angular/flex-layout';
 
 import { ViewerHeaderComponent } from './viewer-header.component';
+import { ContentsComponent } from './../contents/contents.component';
+import { MetadataComponent } from './../contents/metadata/metadata.component';
 import { MimeViewerIntl } from './../viewer-intl';
 import { MimeMaterialModule } from './../../mime-material.module';
 
 describe('ViewerHeaderComponent', () => {
   let component: ViewerHeaderComponent;
   let fixture: ComponentFixture<ViewerHeaderComponent>;
+  let spy: any;
+
+  class MockMedia {
+    isActive(): boolean {
+      return true;
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       imports: [
-        MimeMaterialModule
+        ViewerHeaderTestModule
       ],
-      declarations: [ ViewerHeaderComponent ],
       providers: [
-        MimeViewerIntl
+        MimeViewerIntl,
+        { provide: ObservableMedia, useClass: MockMedia }
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -43,6 +55,29 @@ describe('ViewerHeaderComponent', () => {
       fixture.detectChanges();
 
       expect(button.nativeElement.getAttribute('aria-label')).toBe('Metadata of the publication');
-  }));
+    }));
+
+  it('should open contents dialog', () => {
+    component.openContents();
+
+    expect(component.isContentsDialogOpen).toBeTruthy();
+    ;
 
 });
+
+@NgModule({
+  imports: [
+    NoopAnimationsModule,
+    CommonModule,
+    FlexLayoutModule,
+    MimeMaterialModule,
+  ],
+  declarations: [
+    ViewerHeaderComponent,
+    ContentsComponent,
+    MetadataComponent
+  ],
+  exports: [ViewerHeaderComponent],
+  entryComponents: [ContentsComponent],
+})
+class ViewerHeaderTestModule { }
