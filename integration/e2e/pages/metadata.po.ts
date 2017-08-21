77fc4d6c1f2ede792f978c2cd5c56c9558eb746b
ronnymikalsen <ronny.mikalsen@nb.no>
2017-08-21 15:12:12 +0200

    test(e2e): metadata feature

---
+++ integration/e2e/pages/metadata.po.ts
@@ -0,0 +1,39 @@
+import { protractor } from 'protractor/built';
+import { browser, element, by } from 'protractor';
+import { Utils } from '../helpers/utils';
+
+const utils = new Utils();
+export class MetadataPage {
+
+  async getAll() {
+    const metadatas = [];
+    const el = element.all(by.css('.metadata'));
+    utils.waitForElement(el.first());
+    const count = await el.count();
+    for (let i = 0; i < count; i++) {
+      const metadata = el.get(i);
+      const title = await metadata.element(by.css('.title')).getText();
+      const content = await metadata.element(by.css('.content')).getText();
+      metadatas.push(new Metadata({
+        title: title,
+        content: content
+      }));
+    }
+    return metadatas;
+  }
+}
+
+export class Metadata {
+  public title?: string;
+  public content?: string;
+
+  constructor(public fields?: {
+    title?: string;
+    content?: string;
+  }) {
+    if (fields) {
+      this.title = fields.title || this.title;
+      this.content = fields.content || this.content;
+    }
+  }
+}
--- integration/e2e/step-definitions/content-dialog.steps.ts
+++ integration/e2e/step-definitions/content-dialog.steps.ts
@@ -2,16 +2,19 @@ import { defineSupportCode } from 'cucumber';
 import { expect } from '../helpers/chai-imports';
 
 import { ViewerPage } from './../pages/viewer.po';
+import { MetadataPage } from './../pages/metadata.po';
 
 defineSupportCode(function ({ Given, Then }) {
   const page = new ViewerPage();
+  const metadata = new MetadataPage();
 
   Given(/^the viewer is in metadata view$/, async () => {
     await page.contentsDialogButton().click();
   });
 
   Then(/^descriptive metadata are displayed to the user$/, async () => {
-
+    const m = await metadata.getAll();
+    expect(m.length).to.equal(10);
   });
 
 });
