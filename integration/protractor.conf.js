61ff0426bfd8031d7e3309573296476529ad85a7
ronnymikalsen <ronny.mikalsen@nb.no>
2017-09-01 14:24:25 +0200

    test: selenium version

--- integration/protractor.conf.js
+++ integration/protractor.conf.js
@@ -86,7 +86,6 @@ function getCapabilities() {
       name: 'Mime E2E Tests',
       tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
       build: process.env.TRAVIS_JOB_NUMBER,
-      seleniumVersion: '3.5.0',
       shardTestFiles: true,
       maxInstances: 1,
     });
