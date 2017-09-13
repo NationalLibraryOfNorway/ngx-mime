d2738e7e7d728f4f1c003e3d4ff7933f6a5ede2e
ronnymikalsen <ronny.mikalsen@nb.no>
2017-09-13 12:01:13 +0200

    fix

--- integration/protractor.conf.js
+++ integration/protractor.conf.js
@@ -91,10 +91,7 @@ function getCapabilities() {
       tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
       build: process.env.TRAVIS_JOB_NUMBER,
       seleniumVersion: '3.4.0',
-      screenResolution: "1024x768",
-      nativeEvents: false,
-      ignoreZoomSetting: true,
-      enablePersistentHover: true
+      screenResolution: "1024x768"
     });
   }
   return capabilities;
