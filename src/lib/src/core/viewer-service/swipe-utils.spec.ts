0dccd40f75adfbfca14606ccab9925935cd0806e
Stian Morsund <stian@stianmorsund.no>
2017-09-22 10:59:08 +0200

    test: karma for isPanningPastCenter

--- src/lib/src/core/viewer-service/swipe-utils.spec.ts
+++ src/lib/src/core/viewer-service/swipe-utils.spec.ts
@@ -13,20 +13,62 @@ describe('SwipeUtils ', () => {
     expect(direction).toBe('left');
   });
 
-  it('should be outside right page bounds', () => {
-    let pageBounds = { x: 0, width: 200 };
+  it('should return true when panning outside right page-bounds', () => {
+    const pageBounds = { x: 0, width: 200 };
     // Pan outside right bounds
-    let viewportBounds = { x: 200, width: 100 };
+    const viewportBounds = { x: 200, width: 100 };
 
     expect(SwipeUtils.isPanningOutsidePage(pageBounds, viewportBounds)).toBe(true);
   });
 
-  it('should be outside left page bounds', () => {
-    let pageBounds = { x: 100, width: 200 };
+  it('should return true when panning outside left page-bounds', () => {
+    const pageBounds = { x: 100, width: 200 };
     // Pan outside left bounds
-    let viewportBounds = { x: 99, width: 100 };
+    const viewportBounds = { x: 99, width: 100 };
 
     expect(SwipeUtils.isPanningOutsidePage(pageBounds, viewportBounds)).toBe(true);
+  });
+
+  it('should return false when not panning outside page-bounds', () => {
+    const pageBounds = { x: 0, width: 200 };
+    // Panning inside bounds
+    const viewportBounds = { x: 60, width: 100 };
+
+    expect(SwipeUtils.isPanningOutsidePage(pageBounds, viewportBounds)).toBe(false);
+  });
+
+  it('should return true when panning past center of page on the right side', () => {
+    const pageBounds = { x: 0, width: 100 };
+
+    let viewportBounds = { x: 51, width: 100 };
+    expect(SwipeUtils.isPanningPastCenter(pageBounds, viewportBounds)).toBe(true);
+
+    viewportBounds = { x: 60, width: 200 }
+    expect(SwipeUtils.isPanningPastCenter(pageBounds, viewportBounds)).toBe(true);
+  });
+
+  it('should return true when panning past center of page on the left side', () => {
+    let pageBounds = { x: 0, width: 100 };
+    let viewportBounds = { x: -51, width: 100 };
+
+    expect(SwipeUtils.isPanningPastCenter(pageBounds, viewportBounds)).toBe(true);
+
+    pageBounds = { x: 100, width: 100 };
+    viewportBounds = { x: 49, width: 100 }
+
+    expect(SwipeUtils.isPanningPastCenter(pageBounds, viewportBounds)).toBe(true);
+  });
+
+  it('should return false when not panning past center of page', () => {
+    const pageBounds = { x: 0, width: 100 };
+
+    // Not past center on right side
+    let viewportBounds = { x: 49, width: 100 };
+    expect(SwipeUtils.isPanningPastCenter(pageBounds, viewportBounds)).toBe(false);
+
+    // Not past center on left side
+    viewportBounds = { x: -49, width: 100 };
+    expect(SwipeUtils.isPanningPastCenter(pageBounds, viewportBounds)).toBe(false);
   });
 
 });
