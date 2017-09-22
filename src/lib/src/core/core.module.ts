2553dccd55e0667acd93a53c89c72211f3f95034
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2017-09-22 12:51:26 +0200

    refactor: tslint

--- src/lib/src/core/core.module.ts
+++ src/lib/src/core/core.module.ts
@@ -1,9 +1,9 @@
-import { IiifContentSearchService } from './iiif-content-search-service/iiif-content-search.service';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MimeViewerIntl } from './viewer-intl';
 import { IiifManifestService } from './iiif-manifest-service/iiif-manifest-service';
+import { IiifContentSearchService } from './iiif-content-search-service/iiif-content-search.service';
 import { PageService } from '../core/page-service/page-service';
 import { MimeResizeService } from './mime-resize-service/mime-resize.service';
 import { FullscreenService } from './fullscreen-service/fullscreen.service';
