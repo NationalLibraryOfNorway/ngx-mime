8f64532a612464933b8ddcf7d3159f638546eb56
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2017-10-02 13:20:48 +0200

    BREAKING CHANGE: renamed export of viewer component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
-export { ViewerComponent } from './src/viewer/viewer.component';
+export { ViewerComponent as MimeViewerComponent } from './src/viewer/viewer.component';
 export { MimeViewerIntl } from './src/core/viewer-intl';
 export { MimeModule } from './src/module';
 export { MimeViewerConfig } from './src/core/mime-viewer-config';
+export { ViewerMode as MimeViewerMode } from './src/core/models/viewer-mode';
