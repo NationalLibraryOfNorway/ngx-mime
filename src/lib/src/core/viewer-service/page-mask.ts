2aa3774d09108593b54c5f918a4eeb67117290e6
Ronny Mikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2017-10-10 11:47:41 +0200

    fix: page mask covers entire screen
    fixes https://github.com/NationalLibraryOfNorway/ngx-mime/issues/112

--- src/lib/src/core/viewer-service/page-mask.ts
+++ src/lib/src/core/viewer-service/page-mask.ts
@@ -15,30 +15,6 @@ export class PageMask {
 
   constructor(viewer: any) {
     this.viewer = viewer;
-
-    this.viewer.addHandler('animation', () => {
-      this.resize();
-    });
-
-    this.viewer.addHandler('resize', () => {
-      this.setCenter();
-      this.resize();
-    });
-
-    this.viewer.addHandler('canvas-pinch', () => {
-      this.disableResize = false;
-    });
-
-    this.viewer.addHandler('canvas-drag', (e: any) => {
-      if ((e.delta.x || e.delta.y) && e.speed > 0 && e.direction !== 0) {
-        this.disableResize = true;
-      }
-    });
-
-    this.viewer.addHandler('canvas-drag-end', () => {
-      this.disableResize = false;
-      this.resize();
-    });
   }
 
   public initialise(pageBounds: SVGRectElement): void {
@@ -56,6 +32,7 @@ export class PageMask {
   }
 
   public show(): void {
+    this.addHandlers();
     if (!this.leftMask || !this.rightMask) {
       return;
     }
@@ -64,11 +41,59 @@ export class PageMask {
   }
 
   public hide(): void {
+    this.removeHandlers();
     if (!this.leftMask || !this.rightMask) {
       return;
     }
     this.leftMask.attr('height', '0');
     this.rightMask.attr('height', '0');
+  }
+
+  private addHandlers() {
+    this.viewer.addHandler('animation', this.animationHandler);
+    this.viewer.addHandler('animation-finish', this.animationFinishHandler);
+    this.viewer.addHandler('resize', this.resizeHandler);
+    this.viewer.addHandler('canvas-pinch', this.canvasPinchHandler);
+    this.viewer.addHandler('canvas-drag', this.canvasDragHandler);
+    this.viewer.addHandler('canvas-drag-end', this.canvasDragEndHandler);
+  }
+
+  private removeHandlers() {
+    this.viewer.removeHandler('animation', this.animationHandler);
+    this.viewer.removeHandler('animation-finish', this.animationFinishHandler);
+    this.viewer.removeHandler('resize', this.resizeHandler);
+    this.viewer.removeHandler('canvas-pinch', this.canvasPinchHandler);
+    this.viewer.removeHandler('canvas-drag', this.canvasDragHandler);
+    this.viewer.removeHandler('canvas-drag-end', this.canvasDragEndHandler);
+  }
+
+  private animationHandler = () => {
+    this.resize();
+  }
+
+  private animationFinishHandler = () => {
+    this.setCenter();
+    this.resize();
+  }
+
+  private resizeHandler = () => {
+    this.setCenter();
+    this.resize();
+  }
+
+  private canvasPinchHandler = () => {
+    this.disableResize = false;
+  }
+
+  private canvasDragHandler = (e: any) => {
+    if ((e.delta.x || e.delta.y) && e.speed > 0 && e.direction !== 0) {
+      this.disableResize = true;
+    }
+  }
+
+  private canvasDragEndHandler = () => {
+    this.disableResize = false;
+    this.resize();
   }
 
   private addCanvasMask() {
