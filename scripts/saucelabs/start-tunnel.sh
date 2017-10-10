0555e7806d6bd832fcad33b57d18cd7935029a1c
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2017-10-10 13:48:52 +0200

    ci: updated soucelabs connector

--- scripts/saucelabs/start-tunnel.sh
+++ scripts/saucelabs/start-tunnel.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail
 
-TUNNEL_FILE="sc-4.4.7-linux.tar.gz"
+TUNNEL_FILE="sc-4.4.9-linux.tar.gz"
 TUNNEL_URL="https://saucelabs.com/downloads/${TUNNEL_FILE}"
 TUNNEL_DIR="/tmp/saucelabs-connect"
 
@@ -27,8 +27,8 @@ rm ${TUNNEL_FILE}
 ARGS="--no-proxy-caching"
 
 # Set tunnel-id only on Travis, to make local testing easier.
-if [ ! -z "${TRAVIS_JOB_NUMBER}" ]; then
-  ARGS="${ARGS} --tunnel-identifier ${TRAVIS_JOB_NUMBER}"
+if [ ! -z "${TRAVIS_JOB_ID}" ]; then
+  ARGS="${ARGS} --tunnel-identifier ${TRAVIS_JOB_ID}"
 fi
 if [ ! -z "${BROWSER_PROVIDER_READY_FILE}" ]; then
   ARGS="${ARGS} --readyfile ${BROWSER_PROVIDER_READY_FILE}"
--- scripts/saucelabs/wait-tunnel.sh
+++ scripts/saucelabs/wait-tunnel.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+TUNNEL_LOG="$LOGS_DIR/saucelabs-tunnel.log"
+WAIT_DELAY=30
+
+# Method that prints the logfile output of the saucelabs tunnel.
+printLog() {
+  echo "Logfile output of Saucelabs tunnel (${TUNNEL_LOG}):"
+  echo ""
+  cat ${TUNNEL_LOG}
+}
+
 # Wait for Saucelabs Connect to be ready before exiting
 # Time out if we wait for more than 2 minutes, so the process won't run forever.
 let "counter=0"
@@ -7,14 +17,17 @@ let "counter=0"
 while [ ! -f $BROWSER_PROVIDER_READY_FILE ]; do
   let "counter++"
 
-  if [ $counter -gt 240 ]; then
+  # Counter needs to be multiplied by two because the while loop only sleeps a half second.
+  # This has been made in favor of better progress logging (printing dots every half second)
+  if [ $counter -gt $[${WAIT_DELAY} * 2] ]; then
     echo ""
     echo "Timed out after 2 minutes waiting for tunnel ready file"
+    printLog
     exit 5
   fi
 
   printf "."
-  sleep .5
+  sleep 0.5
 done
 
 echo ""
