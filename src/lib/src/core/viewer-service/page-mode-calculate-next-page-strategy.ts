e05e089b48daf2d32a5d812d4e0ec24f154baab3
Stian Morsund <stian@stianmorsund.no>
2017-10-10 13:11:46 +0200

    fix: better fast swiping in page-mode

--- src/lib/src/core/viewer-service/page-mode-calculate-next-page-strategy.ts
+++ src/lib/src/core/viewer-service/page-mode-calculate-next-page-strategy.ts
@@ -10,10 +10,13 @@ export class PageModeCalculateNextPageStrategy implements CalculateNextPageStrat
     const direction = criteria.direction;
     const currentPageCenter = criteria.currentPageCenter;
 
-    let pageDelta = (speed >= 200 && !isNewPageInCenter) ? 1 : 0;
-    pageDelta = direction === Direction.LEFT ? pageDelta : pageDelta * -1;
-    const nextPage = currentPageCenter + pageDelta;
+    console.log("isNewPageInCenter",isNewPageInCenter)
+    console.log("direction",direction)
 
+    let pageDelta = (speed >= 200 || isNewPageInCenter) ? 1 : 0;
+    pageDelta = direction === Direction.LEFT ? pageDelta : pageDelta * -1;
+    const nextPage = criteria.currentPageIndex + pageDelta;
+    console.log("nextPage", nextPage)
     return nextPage;
   }
 }
