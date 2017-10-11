468d083cf440c9eebb6be27bd981a1c800858e43
ronnymikalsen <ronny.mikalsen@nb.no>
2017-10-11 18:10:08 +0200

    refactor:

--- integration/server/index.js
+++ integration/server/index.js
@@ -6,11 +6,11 @@ const http = require('http');
 const app = express();
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 /**
