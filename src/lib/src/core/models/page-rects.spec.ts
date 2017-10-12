6cdb9db36ccaf8b44ae61bbc2152792784e79100
Catriona MacKenzie <catriona.mackenzie@bouvet.no>
2017-10-12 11:38:39 +0200

    [NGXMIM-15] Fixed failing page-rects karma test

--- src/lib/src/core/models/page-rects.spec.ts
+++ src/lib/src/core/models/page-rects.spec.ts
@@ -1,18 +1,19 @@
 import { PageRects } from './page-rects';
+import { ViewerLayout } from './viewer-layout';
 
 describe('CenterPoints ', () => {
 
   it('should return closest index', () => {
-    const array = [10, 20, 30, 40, 50, 60];
-    const centerPoints = new PageRects();
-    centerPoints.add({ x: 10, y: 0 }, 0);
-    centerPoints.add({ x: 20, y: 0 }, 1);
-    centerPoints.add({ x: 30, y: 0 }, 2);
-    centerPoints.add({ x: 40, y: 0 }, 3);
-    centerPoints.add({ x: 50, y: 0 }, 4);
-    centerPoints.add({ x: 60, y: 0 }, 5);
+    const tileRects = [];
+    tileRects.push({ x: 10, y: 0 });
+    tileRects.push({ x: 20, y: 0 });
+    tileRects.push({ x: 30, y: 0 });
+    tileRects.push({ x: 40, y: 0 });
+    tileRects.push({ x: 50, y: 0 });
+    tileRects.push({ x: 60, y: 0 });
 
-    const index = centerPoints.findClosestIndex({ x: 56, y: 0 });
+    const pageRects = new PageRects(tileRects, ViewerLayout.ONE_PAGE, false);
+    const index = pageRects.findClosestIndex({ x: 56, y: 0 });
 
     expect(index).toBe(5);
   });
