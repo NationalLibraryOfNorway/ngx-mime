7766f9a84133376f24ce1e34672cc00347a60372
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2017-10-23 14:36:39 +0200

    feat: keyboard should be closed on mobile after content search
    fixes #150

--- src/lib/src/test/iiif-manifest-service-stub.ts
+++ src/lib/src/test/iiif-manifest-service-stub.ts
@@ -8,15 +8,40 @@ import { Manifest } from './../core/models/manifest';
 
 export class IiifManifestServiceStub {
   public _currentManifest: Subject<Manifest> = new BehaviorSubject<Manifest>(new Manifest());
+  public _errorMessage: Subject<string> = new BehaviorSubject(null);
 
   get currentManifest(): Observable<Manifest> {
     return this._currentManifest.asObservable();
   }
 
-  load(manifestUri: string): void {
-    if (manifestUri === null) {
-      return;
-    }
-    this._currentManifest.next(new ManifestBuilder(testManifest).build());
+  get errorMessage(): Observable<string> {
+    return this._errorMessage.asObservable();
   }
+
+  load(manifestUri: string): void {
+    if (manifestUri) {
+      const manifest = new ManifestBuilder(testManifest).build();
+      if (manifest && manifest.tileSource) {
+        this._currentManifest.next(manifest);
+      } else {
+        this._errorMessage.next('Manifest is not valid');
+      }
+    } else {
+      this._errorMessage.next('ManifestUri is missing');
+    }
+  }
+
+  resetCurrentManifest() {
+    this._currentManifest.next(null);
+  }
+
+  resetErrorMessage() {
+    this._errorMessage.next(null);
+  }
+
+  destroy(): void { }
+
+  public search(manifest: Manifest, q: string): void {
+  }
+
 }
