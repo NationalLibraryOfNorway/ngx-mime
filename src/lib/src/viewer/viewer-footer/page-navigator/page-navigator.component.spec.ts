d4692ae277c4e84ee827e02f90fd349b2acdb410
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2017-12-19 09:49:00 +0100

    refactor: moved import

--- src/lib/src/viewer/viewer-footer/page-navigator/page-navigator.component.spec.ts
+++ src/lib/src/viewer/viewer-footer/page-navigator/page-navigator.component.spec.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
@@ -21,7 +22,6 @@ import { IiifContentSearchService } from './../../../core/iiif-content-search-se
 import { Hit } from './../../../core/models/search-result';
 import { ViewerServiceMock } from './../../../test/viewer-service-mock';
 import { PageDialogService } from '../../../page-dialog/page-dialog.service';
-import { ReactiveFormsModule } from '@angular/forms';
 
 describe('PageNavigatorComponent', () => {
   let component: PageNavigatorComponent;
