7b3e566ddce75082e70bf044c20e489b4d4491a6
raymondkarstensen <raymond.karstensen@nb.no>
2018-02-05 11:59:39 +0100

    fix: Wrong order of imports

--- src/lib/src/core/access-keys-handler-service/access-keys.service.spec.ts
+++ src/lib/src/core/access-keys-handler-service/access-keys.service.spec.ts
@@ -1,18 +1,19 @@
-import { ViewerService } from '../viewer-service/viewer.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { inject, TestBed } from '@angular/core/testing';
-import { ContentSearchDialogService } from '../../content-search-dialog/content-search-dialog.service';
+
+import { SharedModule } from '../../shared/shared.module';
+import { MimeViewerIntl } from '../intl/viewer-intl';
+import { MimeDomHelper } from '../mime-dom-helper';
+import { ViewerService } from '../viewer-service/viewer.service';
 import { ClickService } from '../click-service/click.service';
 import { PageService } from '../page-service/page-service';
 import { ModeService } from '../mode-service/mode.service';
+import { AccessKeysService } from './access-keys.service';
+import { ContentSearchDialogService } from '../../content-search-dialog/content-search-dialog.service';
 import { IiifManifestService } from '../iiif-manifest-service/iiif-manifest-service';
 import { ContentsDialogService } from '../../contents-dialog/contents-dialog.service';
-import { AccessKeysService } from './access-keys.service';
-import { MimeViewerIntl } from '../intl/viewer-intl';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { SharedModule } from '../../shared/shared.module';
 import { ContentsDialogConfigStrategyFactory } from '../../contents-dialog/contents-dialog-config-strategy-factory';
 import { ContentSearchDialogConfigStrategyFactory } from '../../content-search-dialog/content-search-dialog-config-strategy-factory';
-import { MimeDomHelper } from '../mime-dom-helper';
 import { FullscreenService } from '../fullscreen-service/fullscreen.service';
 import { MimeResizeService } from '../mime-resize-service/mime-resize.service';
 import { ViewerLayoutService } from '../viewer-layout-service/viewer-layout-service';
