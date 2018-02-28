006cdeef8a30b00142576b7cdffa68553a888bce
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2018-02-28 12:44:13 +0100

    refactor: rename to canvasgroup

--- src/lib/src/core/canvas-service/canvas-service.spec.ts
+++ src/lib/src/core/canvas-service/canvas-service.spec.ts
@@ -15,27 +15,27 @@ describe('PageService', () => {
     service.addAll(pages, ViewerLayout.ONE_PAGE);
   });
 
-  it('#isWithinBounds should return true when requested page is within bounds', () => {
+  it('#isWithinBounds should return true when requested canvas group index is within bounds', () => {
     expect(service.isWithinBounds(0)).toBe(true);
     expect(service.isWithinBounds(10)).toBe(true);
     expect(service.isWithinBounds(99)).toBe(true);
   });
 
-  it('#isWithinBounds should return false when requested page is outside bounds', () => {
+  it('#isWithinBounds should return false when requested canvas group index is outside bounds', () => {
     expect(service.isWithinBounds(-1)).toBe(false);
     expect(service.isWithinBounds(100)).toBe(false);
     expect(service.isWithinBounds(101)).toBe(false);
     expect(service.isWithinBounds(1000)).toBe(false);
   });
 
-  it('should set currentPage', () => {
+  it('should set canvas group index', () => {
     service.currentCanvasGroupIndex = 0;
     expect(service.currentCanvasGroupIndex).toBe(0);
     service.currentCanvasGroupIndex = 99;
     expect(service.currentCanvasGroupIndex).toBe(99);
   });
 
-  it('should not set currentPage if outside bounds', () => {
+  it('should not set canvas group index if outside bounds', () => {
     service.currentCanvasGroupIndex = 76;
 
     service.currentCanvasGroupIndex = -2;
@@ -51,7 +51,7 @@ describe('PageService', () => {
     expect(service.currentCanvasGroupIndex).toBe(76);
   });
 
-  it('#getNextPage should get next page', () => {
+  it('should get next canvas group index', () => {
     let currentPage = (service.currentCanvasGroupIndex = 0);
     expect(service.getNextCanvasGroupIndex()).toBe(currentPage + 1);
 
@@ -59,7 +59,7 @@ describe('PageService', () => {
     expect(service.getNextCanvasGroupIndex()).toBe(currentPage + 1);
   });
 
-  it('#getPrevPage should get previous page', () => {
+  it('should get previous canvas group index', () => {
     let currentPage = (service.currentCanvasGroupIndex = 2);
     expect(service.getPrevCanvasGroupIndex()).toBe(currentPage - 1);
 
@@ -67,37 +67,37 @@ describe('PageService', () => {
     expect(service.getPrevCanvasGroupIndex()).toBe(currentPage - 1);
   });
 
-  it('#getNextPage should return -1 when going out of bounds', () => {
+  it('should return -1 when next canvas group index is out of bounds', () => {
     service.currentCanvasGroupIndex = 99;
     expect(service.getNextCanvasGroupIndex()).toBe(-1);
   });
 
-  it('#getPrevPage should return -1 when going out of bounds', () => {
+  it('should return -1 when previous canvas group index is out of bounds', () => {
     service.currentCanvasGroupIndex = 0;
     expect(service.getPrevCanvasGroupIndex()).toBe(-1);
   });
 
-  it('should return maxPage when next page is larger than maxPage', () => {
-    let newPage = service.constrainToRange(101);
-    expect(newPage).toBe(99);
+  it('should return max canvas group index when next canvas group index is larger than max', () => {
+    let newCanvasGroupIndex = service.constrainToRange(101);
+    expect(newCanvasGroupIndex).toBe(99);
 
-    newPage = service.constrainToRange(110);
-    expect(newPage).toBe(99);
+    newCanvasGroupIndex = service.constrainToRange(110);
+    expect(newCanvasGroupIndex).toBe(99);
   });
 
-  it('should not return index lower than 0', () => {
-    let newPage = service.constrainToRange(0);
-    expect(newPage).toBe(0);
+  it('should not return canvas group index lower than 0', () => {
+    let newCanvasGroupIndex = service.constrainToRange(0);
+    expect(newCanvasGroupIndex).toBe(0);
 
-    newPage = service.constrainToRange(-1);
-    expect(newPage).toBe(0);
+    newCanvasGroupIndex = service.constrainToRange(-1);
+    expect(newCanvasGroupIndex).toBe(0);
 
-    newPage = service.constrainToRange(-10);
-    expect(newPage).toBe(0);
+    newCanvasGroupIndex = service.constrainToRange(-10);
+    expect(newCanvasGroupIndex).toBe(0);
   });
 
-  it('should return 1 if tileIndicesPerPage is empty', () => {
-    const page = service.getCanvasGroupLabel(0);
-    expect(page).toBe('1');
+  it('should return 1 if canvas group is empty', () => {
+    const canvasGroupLabel = service.getCanvasGroupLabel(0);
+    expect(canvasGroupLabel).toBe('1');
   });
 });
