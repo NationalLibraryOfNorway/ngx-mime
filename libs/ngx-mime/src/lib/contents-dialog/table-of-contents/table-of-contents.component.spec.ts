cbeea52b07d2b37070b5eb0958ee4329f5d86638
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2019-01-14 11:00:22 +0100

    refactor: tests

--- libs/ngx-mime/src/lib/contents-dialog/table-of-contents/table-of-contents.component.spec.ts
+++ libs/ngx-mime/src/lib/contents-dialog/table-of-contents/table-of-contents.component.spec.ts
@@ -1,11 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { DebugElement } from '@angular/core';
-import {
-  async,
-  ComponentFixture,
-  inject,
-  TestBed
-} from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MediaObserver } from '@angular/flex-layout';
 import { MatDialogRef } from '@angular/material';
 import { By } from '@angular/platform-browser';
@@ -19,8 +14,6 @@ import { ViewerService } from '../../core/viewer-service/viewer.service';
 import { SharedModule } from '../../shared/shared.module';
 import { IiifManifestServiceStub } from '../../test/iiif-manifest-service-stub';
 import { MatDialogRefStub } from '../../test/mat-dialog-ref-stub';
-import { MediaObserverStub } from '../../test/media-observer-stub';
-import { ContentsDialogComponent } from '../contents-dialog.component';
 import { ViewerServiceStub } from './../../test/viewer-service-stub';
 import { TocComponent } from './table-of-contents.component';
 
@@ -28,6 +21,9 @@ describe('TocComponent', () => {
   let component: TocComponent;
   let fixture: ComponentFixture<TocComponent>;
   let iiifManifestService: IiifManifestServiceStub;
+  let mediaObserver: any;
+  let viewerService: ViewerService;
+  let dialogRef: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -39,7 +35,6 @@ describe('TocComponent', () => {
         ModeService,
         MimeViewerIntl,
         { provide: MatDialogRef, useClass: MatDialogRefStub },
-        { provide: MediaObserver, useClass: MediaObserverStub },
         { provide: IiifManifestService, useClass: IiifManifestServiceStub },
         { provide: ViewerService, useClass: ViewerServiceStub }
       ]
@@ -50,6 +45,7 @@ describe('TocComponent', () => {
     fixture = TestBed.createComponent(TocComponent);
     component = fixture.componentInstance;
     iiifManifestService = TestBed.get(IiifManifestService);
+    viewerService = TestBed.get(ViewerService);
 
     iiifManifestService._currentManifest.next(
       new Manifest({
@@ -83,6 +79,8 @@ describe('TocComponent', () => {
         ]
       })
     );
+    mediaObserver = TestBed.get(MediaObserver);
+    dialogRef = TestBed.get(MatDialogRef);
 
     fixture.detectChanges();
   });
@@ -118,35 +116,26 @@ describe('TocComponent', () => {
     expect(canvasGroupNumbers[2].nativeElement.innerText).toEqual('5');
   });
 
-  it('should go to canvas group when selecting a canvas group in TOC', inject(
-    [ViewerService],
-    (viewerService: ViewerService) => {
-      spyOn(viewerService, 'goToCanvas').and.callThrough();
+  it('should go to canvas group when selecting a canvas group in TOC', () => {
+    spyOn(viewerService, 'goToCanvas').and.callThrough();
 
-      const divs: DebugElement[] = fixture.debugElement.queryAll(
-        By.css('.toc-link')
-      );
-      divs[2].triggerEventHandler('click', null);
+    const divs: DebugElement[] = fixture.debugElement.queryAll(
+      By.css('.toc-link')
+    );
+    divs[2].triggerEventHandler('click', null);
 
-      expect(viewerService.goToCanvas).toHaveBeenCalledWith(4, false);
-    }
-  ));
+    expect(viewerService.goToCanvas).toHaveBeenCalledWith(4, false);
+  });
 
-  it('should close contents dialog when selecting a canvas group in TOC when on mobile', inject(
-    [MatDialogRef, MediaObserver],
-    (
-      dialogRef: MatDialogRef<ContentsDialogComponent>,
-      mediaObserver: MediaObserver
-    ) => {
-      spyOn(mediaObserver, 'isActive').and.returnValue(true);
-      spyOn(dialogRef, 'close').and.callThrough();
+  it('should close contents dialog when selecting a canvas group in TOC when on mobile', () => {
+    spyOn(mediaObserver, 'isActive').and.returnValue(true);
+    spyOn(dialogRef, 'close').and.callThrough();
 
-      const divs: DebugElement[] = fixture.debugElement.queryAll(
-        By.css('.toc-link')
-      );
-      divs[2].triggerEventHandler('click', null);
+    const divs: DebugElement[] = fixture.debugElement.queryAll(
+      By.css('.toc-link')
+    );
+    divs[2].triggerEventHandler('click', null);
 
-      expect(dialogRef.close).toHaveBeenCalled();
-    }
-  ));
+    expect(dialogRef.close).toHaveBeenCalled();
+  });
 });
--- libs/ngx-mime/src/lib/core/viewer-layout-service/viewer-layout-service.spec.ts
+++ libs/ngx-mime/src/lib/core/viewer-layout-service/viewer-layout-service.spec.ts
@@ -1,73 +1,55 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { MediaObserver } from '@angular/flex-layout';
-
+import { MimeViewerConfig } from '../mime-viewer-config';
 import { ViewerLayout } from '../models/viewer-layout';
 import { ViewerLayoutService } from './viewer-layout-service';
-import { MimeViewerConfig } from '../mime-viewer-config';
-import { MediaObserverStub } from '../../test/media-observer-stub';
 
 describe('ViewerLayoutService', () => {
   let config: MimeViewerConfig;
+  let service: ViewerLayoutService;
+  let mediaObserver: any;
+
   beforeEach(() => {
     config = new MimeViewerConfig();
     TestBed.configureTestingModule({
-      providers: [
-        ViewerLayoutService,
-        { provide: MediaObserver, useClass: MediaObserverStub }
-      ]
+      providers: [ViewerLayoutService]
     });
+    service = TestBed.get(ViewerLayoutService);
+    mediaObserver = TestBed.get(MediaObserver);
   });
 
-  it('should be created', inject(
-    [ViewerLayoutService],
-    (service: ViewerLayoutService) => {
-      expect(service).toBeTruthy();
-    }
-  ));
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
-  it('should emit when ViewerLayout is toggled', inject(
-    [ViewerLayoutService],
-    (service: ViewerLayoutService) => {
-      let newLayout: ViewerLayout;
-      service.onChange.subscribe((state: ViewerLayout) => (newLayout = state));
-      service.setLayout(ViewerLayout.ONE_PAGE);
-      expect(newLayout).toEqual(ViewerLayout.ONE_PAGE);
-      service.setLayout(ViewerLayout.TWO_PAGE);
-      expect(newLayout).toEqual(ViewerLayout.TWO_PAGE);
-    }
-  ));
+  it('should emit when ViewerLayout is toggled', () => {
+    let newLayout: ViewerLayout;
+    service.onChange.subscribe((state: ViewerLayout) => (newLayout = state));
+    service.setLayout(ViewerLayout.ONE_PAGE);
+    expect(newLayout).toEqual(ViewerLayout.ONE_PAGE);
+    service.setLayout(ViewerLayout.TWO_PAGE);
+    expect(newLayout).toEqual(ViewerLayout.TWO_PAGE);
+  });
 
-  it('should set initial layout to specified layout in mime-config', inject(
-    [ViewerLayoutService, MediaObserver],
-    (service: ViewerLayoutService, mediaObserver: MediaObserver) => {
-      const initLayout = config.initViewerLayout;
-      spyOn(mediaObserver, 'isActive').and.returnValue(false);
-      service.init();
+  it('should set initial TWO_PAGE layout to specified layout in mime-config', () => {
+    const initLayout = config.initViewerLayout;
+    spyOn(mediaObserver, 'isActive').and.returnValue(false);
+    service.init();
 
-      if (initLayout === ViewerLayout.TWO_PAGE) {
-        expect(service.layout === ViewerLayout.TWO_PAGE);
-      }
-      if (initLayout === ViewerLayout.ONE_PAGE) {
-        expect(service.layout === ViewerLayout.ONE_PAGE);
-      }
+    if (initLayout === ViewerLayout.TWO_PAGE) {
+      expect(service.layout === ViewerLayout.TWO_PAGE);
     }
-  ));
+  });
 
-  it('should set initial layout to ONE_PAGE on mobile, regardless of mime-config', inject(
-    [ViewerLayoutService, MediaObserver],
-    (service: ViewerLayoutService, mediaObserver: MediaObserver) => {
-      spyOn(mediaObserver, 'isActive').and.returnValue(true);
-      service.init();
-      expect(service.layout).toEqual(ViewerLayout.ONE_PAGE);
-    }
-  ));
+  it('should set initial layout to ONE_PAGE on mobile, regardless of mime-config', () => {
+    spyOn(mediaObserver, 'isActive').and.returnValue(true);
+    service.init();
+    expect(service.layout).toEqual(ViewerLayout.ONE_PAGE);
+  });
 
-  it('should set initial layout to TWO_PAGE if manifest is paged AND we are not and mobile', inject(
-    [ViewerLayoutService, MediaObserver],
-    (service: ViewerLayoutService, mediaObserver: MediaObserver) => {
-      spyOn(mediaObserver, 'isActive').and.returnValue(false);
-      service.init(true);
-      expect(service.layout).toEqual(ViewerLayout.TWO_PAGE);
-    }
-  ));
+  it('should set initial layout to TWO_PAGE if manifest is paged AND we are not and mobile', () => {
+    spyOn(mediaObserver, 'isActive').and.returnValue(false);
+    service.init(true);
+    expect(service.layout).toEqual(ViewerLayout.TWO_PAGE);
+  });
 });
