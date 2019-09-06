8c7ea413f8baea161f92086374c5f2e3ed488935
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2019-09-06 12:18:28 +0200

    test: fix

--- apps/integration/src/app/viewer/viewer.component.spec.ts
+++ apps/integration/src/app/viewer/viewer.component.spec.ts
@@ -1,8 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ViewerComponent } from './viewer.component';
 import { MimeModule } from '@nationallibraryofnorway/ngx-mime';
+import { ViewerComponent } from './viewer.component';
 
 describe('ViewerComponent', () => {
   let component: ViewerComponent;
@@ -10,7 +11,12 @@ describe('ViewerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MimeModule, RouterTestingModule, HttpClientTestingModule],
+      imports: [
+        MimeModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        NoopAnimationsModule
+      ],
       declarations: [ViewerComponent]
     }).compileComponents();
   }));
