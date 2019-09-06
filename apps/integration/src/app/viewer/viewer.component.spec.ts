f92a43f341c0718b9c7461b30043e426965d710f
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2019-09-06 09:19:45 +0200

    test: fix karma test deps

--- apps/integration/src/app/viewer/viewer.component.spec.ts
+++ apps/integration/src/app/viewer/viewer.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ViewerComponent } from './viewer.component';
 import { MimeModule } from '@nationallibraryofnorway/ngx-mime';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ViewerComponent', () => {
   let component: ViewerComponent;
@@ -10,7 +11,12 @@ describe('ViewerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MimeModule, RouterTestingModule, HttpClientTestingModule],
+      imports: [
+        MimeModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        NoopAnimationsModule
+      ],
       declarations: [ViewerComponent]
     }).compileComponents();
   }));
