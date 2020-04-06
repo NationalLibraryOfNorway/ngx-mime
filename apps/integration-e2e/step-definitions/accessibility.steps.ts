30338ace60fd686d515b33d27764c82b451463d2
Ronny Mikalsen <ronny.mikalsen@nb.no>
2020-04-06 08:24:39 +0200

    test(e2e): a11y

--- apps/integration-e2e/step-definitions/accessibility.steps.ts
+++ apps/integration-e2e/step-definitions/accessibility.steps.ts
@@ -6,7 +6,7 @@ const AxeBuilder = require('axe-webdriverjs');
 
 Then('the viewer should meet all accessibility criteria', async () => {
   await AxeBuilder(browser.driver)
-    .disableRules('page-has-heading-one')
+    .disableRules(['page-has-heading-one', 'landmark-one-main'])
     .analyze()
     .then(results => {
       results.violations.forEach(v => v.nodes.forEach(n => console.log(n)));
