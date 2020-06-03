1a37689c4dcdf80c407447fb999426fe29a7ce8d
Tomas Šakočius <tomas.sakocius@asseco.lt>
2020-06-03 13:02:09 +0300

    feat: Added lithuanian label translations

---
+++ libs/ngx-mime/src/lib/core/intl/help-intl.lt.ts
@@ -0,0 +1,19 @@
+import { HelpIntl } from './help-intl';
+
+export class HelpIntlLt extends HelpIntl {
+  helpLabel = 'Pagalba';
+  line1 =
+    '<strong>RODYKLĖ KAIRĖN</strong> arba <strong>PAGE UP</strong>: Buvęs puslapis';
+  line2 =
+    '<strong>RODYKLĖ DEŠINĖN</strong> arba <strong>PAGE DOWN</strong>: Kitas puslapis';
+  line3 = '<strong>HOME</strong>: Pirmas puslapis';
+  line4 = '<strong>END</strong>: Paskutinis puslapis';
+  line5 =
+    '<strong>C</strong>: Turinio langas su daugiau informacijos apie objektą. (Uždaromas paspaudus <strong>ESC</strong>.)';
+  line6 =
+    '<strong>S</strong>: Paieška objekto viduje. (Uždaroma paspaudus <strong>ESC</strong>.)';
+  line7 = '<strong>N</strong>: Kitas rezultatas';
+  line8 = '<strong>P</strong>: Buvęs rezultatas';
+  line9 =
+    '<strong>F</strong>: Pilno ekrano režimas (Uždaroma paspaudus <strong>F</strong> arba <strong>ESC</strong>.)';
+}
