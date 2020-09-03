ef22e88df9a9f31cb36492225ece4c454dbc3b46
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2020-09-03 10:11:47 +0200

    build: refactor

--- apps/integration-e2e/step-definitions/elements.steps.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { Given, Then, When } from 'cucumber';
-import { ElementsPage } from '../pages/elements.po';
-import { ViewerPage } from '../pages/viewer.po';
-import { browser } from 'protractor';
-
-const elementsPage = new ElementsPage();
-const viewerPage = new ViewerPage();
