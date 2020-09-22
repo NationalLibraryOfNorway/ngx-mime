dc4366f1928b3f5148b91309db236a2d15d7fb6f
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2020-09-22 09:36:26 +0200

    style: remove unused variable

--- apps/integration/src/app/viewer/viewer.module.ts
+++ apps/integration/src/app/viewer/viewer.module.ts
@@ -1,12 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
 import { ComponentsModule } from './components/components.module';
 import { ElementsModule } from './elements/elements.module';
 import { ViewerRoutingModule } from './viewer-routing.module';
 import { ViewerComponent } from './viewer.component';
-
-const appRoutes: Routes = [{ path: '', component: ViewerComponent }];
 
 @NgModule({
   declarations: [ViewerComponent],
