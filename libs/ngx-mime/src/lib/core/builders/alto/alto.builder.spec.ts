6c0d9859e551630ade810d9a6345c3cf034f68fb
ronnymikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2021-07-13 14:20:34 +0200

    refactor: tests

--- libs/ngx-mime/src/lib/core/builders/alto/alto.builder.spec.ts
+++ libs/ngx-mime/src/lib/core/builders/alto/alto.builder.spec.ts
@@ -6,24 +6,15 @@ describe('AltoBuilder', () => {
   it('should build altoxml', () => {
     parseString(testAlto, {}, (error, result) => {
       const alto = new AltoBuilder().withAltoXml(result.alto).build();
-      expect(alto).toBeDefined();
-      expect(alto?.layout.page.printSpace.textBlocks.length).toBe(4);
-      expect(alto?.layout.page.printSpace.textBlocks[0].textLines.length).toBe(
-        1
-      );
-      expect(
-        alto?.layout.page.printSpace.textBlocks[0].textLines[0].strings.length
-      ).toBe(4);
-      expect(
-        alto?.layout.page.printSpace.textBlocks[0].textLines[0].strings[0]
-          .content
-      ).toBe('Brann-');
-      expect(
-        alto?.layout.page.printSpace.textBlocks[0].textStyle
-      ).toBeDefined ();
-      expect(
-        alto?.layout.page.printSpace.textBlocks[1].textStyle?.fontStyle
-      ).toBe('bold');
+
+      const textBlocks = alto.layout.page.printSpace.textBlocks;
+      const firstTextBlock = textBlocks[0];
+      const secondTextBlock = textBlocks[1];
+      expect(textBlocks.length).toBe(4);
+      expect(firstTextBlock.textLines.length).toBe(1);
+      expect(firstTextBlock.textLines[0].strings.length).toBe(4);
+      expect(firstTextBlock.textLines[0].strings[0].content).toBe('Brann-');
+      expect(secondTextBlock.textStyle?.fontStyle).toBe('bold');
     });
   });
 });
