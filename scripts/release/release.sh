ef3d01d0b114283694ca5286ccd3ad9bcbd66593
Ronny Mikalsen <ronny.mikalsen@nasjonalbiblioteket.org>
2021-09-16 11:05:30 +0200

    fix: release script

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -12,14 +12,17 @@ cd $(dirname $0)/../..
 git checkout main; git pull origin main
 yarn build:libs
 yarn build:elements
-CURRENT_VERSION=$(node -p "require('./package.json').version")
-cd dist/libs/@nationallibraryofnorway/ngx-mime
 
-echo "Version: $CURRENT_VERSION"
-npm version $CURRENT_VERSION
-
-cd ../../../..
 cp README.md dist/libs/@nationallibraryofnorway/ngx-mime
+CURRENT_VERSION=$(node -p "require('./package.json').version")
+echo "Version: $CURRENT_VERSION"
+
+cd dist/libs/@nationallibraryofnorway/ngx-mime
+npm version $CURRENT_VERSION
+cd ../../../..
+
 git add -f dist && standard-version -a
 git push --follow-tags origin main
+
+npm login
 npm publish dist/libs/@nationallibraryofnorway/ngx-mime
