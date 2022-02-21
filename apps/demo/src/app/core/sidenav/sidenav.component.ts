f0ed754ae05dd7f552e24e61ee59061507303874
Ronny Valentin Vindenes <ronny.vindenes@bouvet.no>
2022-02-21 12:51:13 +0100

    feat: switch loaded manifest when switch iiif api version

--- apps/demo/src/app/core/sidenav/sidenav.component.ts
+++ apps/demo/src/app/core/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 import { ManifestMenuItem } from './../../models/manifest-menu-item.model';
 import { ManifestService } from './../manifest-service/manifest.service';
 
@@ -12,17 +13,32 @@ export class SidenavComponent {
   @Input() sidenav!: MatSidenav;
   iiifV = 3;
   manifests: ManifestMenuItem[];
+  selectedManifest: string | undefined;
 
-  constructor(private manifestService: ManifestService) {
+  constructor(
+    private manifestService: ManifestService,
+    private router: Router
+  ) {
     this.manifests = this.manifestService.getManifests(this.iiifV);
   }
 
   selectIiifV(apiV: number) {
     this.iiifV = apiV;
     this.manifests = this.manifestService.getManifests(this.iiifV);
+    if (this.selectedManifest) {
+      const manifest = this.manifests.find(
+        (m) => m.label === this.selectedManifest
+      );
+      if (manifest?.uri) {
+        this.router.navigate(['demo'], {
+          queryParams: { manifestUri: manifest.uri, v: manifest.iiifVersion },
+        });
+      }
+    }
   }
 
-  close() {
+  close(label?: string) {
+    this.selectedManifest = label;
     if (this.sidenav.mode === 'over') {
       this.sidenav.close();
     }
--- apps/demo/src/app/viewer/viewer.component.ts
+++ apps/demo/src/app/viewer/viewer.component.ts
@@ -23,6 +23,7 @@ export class ViewerComponent implements OnDestroy {
     initViewerMode: MimeViewerMode.PAGE,
     initRecognizedTextContentToggle: false,
   });
+  private iiifV = 3;
   private subscriptions = new Subscription();
 
   constructor(
@@ -33,10 +34,11 @@ export class ViewerComponent implements OnDestroy {
     this.subscriptions.add(
       this.route.queryParams.subscribe((params) => {
         this.manifestUri = params['manifestUri'];
+        this.iiifV = params['v'];
         if (!this.manifestUri) {
           this.router.navigate(['demo'], {
             queryParams: {
-              manifestUri: this.manifestService.getManifests()[0].uri,
+              manifestUri: this.manifestService.getManifests(this.iiifV)[0].uri,
             },
           });
         }
