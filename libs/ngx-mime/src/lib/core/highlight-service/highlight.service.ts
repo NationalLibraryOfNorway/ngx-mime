17b69999f95cc7b44e2eee32ad803a684a01a7e7
Daniel-Taranger <daniel.taranger@bouvet.no>
2022-03-28 13:12:03 +0200

    added optional replace

--- libs/ngx-mime/src/lib/core/highlight-service/highlight.service.ts
+++ libs/ngx-mime/src/lib/core/highlight-service/highlight.service.ts
@@ -29,7 +29,7 @@ export class HighlightService {
 
   private markHtml(html: string, pattern: string, id?: number): string {
     const wordBoundary = '\\b';
-    return html.replace(
+    return html?.replace(
       new RegExp(
         wordBoundary + this.escapeSpecialCharacters(pattern) + '(?!<)'
       ),
