4d05fecdf1bd10813fb61eb6ef25c3e49cc856ec
Ronny Mikalsen <ronny.mikalsen@nb.no>
2022-11-22 12:26:53 +0100

    fix: typo

--- libs/ngx-mime/src/lib/core/builders/alto/alto.builder.spec.ts
+++ libs/ngx-mime/src/lib/core/builders/alto/alto.builder.spec.ts
@@ -47,7 +47,7 @@ describe('AltoBuilder', () => {
     );
   });
 
-  it('should build altoxml with empty margins', () => {
+  it('should build altoxml with no margins', () => {
     parseString(
       testAltoWithNoMargins,
       { preserveChildrenOrder: true, explicitChildren: true },
