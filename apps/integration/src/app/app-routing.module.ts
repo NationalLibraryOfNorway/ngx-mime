c8075d61634bb5c9a2f04ce3e151039f64abe131
Ronny Mikalsen <ronny.mikalsen@nb.no>
2023-01-13 13:39:05 +0100

    chore: format

--- apps/integration/src/app/app-routing.module.ts
+++ apps/integration/src/app/app-routing.module.ts
@@ -15,9 +15,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(appRoutes, {}),
-  ],
+  imports: [RouterModule.forRoot(appRoutes, {})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
