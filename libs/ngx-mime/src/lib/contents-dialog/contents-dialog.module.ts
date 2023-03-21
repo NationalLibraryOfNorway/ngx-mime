72d8ddb6bdbc2f50f0cc195ae6954b6157358bb3
Ronny Mikalsen <ronny.mikalsen@nb.no>
2023-03-21 12:36:16 +0100

    fix: remove ErrorStateMatcher
    Use default behavior instead

--- libs/ngx-mime/src/lib/contents-dialog/contents-dialog.module.ts
+++ libs/ngx-mime/src/lib/contents-dialog/contents-dialog.module.ts
@@ -1,23 +1,15 @@
 import { NgModule } from '@angular/core';
-import {
-  ErrorStateMatcher,
-  ShowOnDirtyErrorStateMatcher,
-} from '@angular/material/core';
 
 import { SharedModule } from './../shared/shared.module';
-import { ContentsDialogService } from './contents-dialog.service';
 import { ContentsDialogConfigStrategyFactory } from './contents-dialog-config-strategy-factory';
 import { ContentsDialogComponent } from './contents-dialog.component';
+import { ContentsDialogService } from './contents-dialog.service';
 import { MetadataComponent } from './metadata/metadata.component';
 import { TocComponent } from './table-of-contents/table-of-contents.component';
 
 @NgModule({
   imports: [SharedModule],
   declarations: [ContentsDialogComponent, MetadataComponent, TocComponent],
-  providers: [
-    ContentsDialogService,
-    ContentsDialogConfigStrategyFactory,
-    { provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher },
-  ],
+  providers: [ContentsDialogService, ContentsDialogConfigStrategyFactory],
 })
 export class ContentsDialogModule {}
