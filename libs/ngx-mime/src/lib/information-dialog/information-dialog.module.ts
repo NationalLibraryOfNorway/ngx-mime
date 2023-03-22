3e8d52476d336624cff397fad0ac0aea134324b4
Raymond Karstensen <raymond.karstensen@nb.no>
2023-03-22 11:59:45 +0100

    fix: imports and style guide after merge

--- libs/ngx-mime/src/lib/information-dialog/information-dialog.module.ts
+++ libs/ngx-mime/src/lib/information-dialog/information-dialog.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { SharedModule } from './../shared/shared.module';
 import { InformationDialogService } from './information-dialog.service';
 import { InformationDialogConfigStrategyFactory } from './information-dialog-config-strategy-factory.service';
@@ -10,9 +9,6 @@ import { TocComponent } from './table-of-contents/table-of-contents.component';
 @NgModule({
   imports: [SharedModule],
   declarations: [InformationDialogComponent, MetadataComponent, TocComponent],
-  providers: [
-    InformationDialogService,
-    InformationDialogConfigStrategyFactory,
-  ],
+  providers: [InformationDialogService, InformationDialogConfigStrategyFactory],
 })
 export class InformationDialogModule {}
