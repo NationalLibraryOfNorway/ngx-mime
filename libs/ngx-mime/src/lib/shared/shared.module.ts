d4012a575ce5b7df95ae0cfaaf763091464ff574
Ronny Mikalsen <ronny.mikalsen@nb.no>
2023-04-25 13:49:39 +0200

    style: removed empty line

--- libs/ngx-mime/src/lib/shared/shared.module.ts
+++ libs/ngx-mime/src/lib/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { MimeMaterialModule } from './mime-material.module';
 
 @NgModule({
