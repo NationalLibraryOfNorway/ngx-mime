718b32a3883d6baac2c5a255fdf4673e1b2f5b4c
Ronny Mikalsen <ronny.mikalsen@nb.no>
2023-07-21 10:08:07 +0200

    fix: removed FlexLayout from integration app spec file

--- apps/integration/src/app/viewer/viewer.component.spec.ts
+++ apps/integration/src/app/viewer/viewer.component.spec.ts
@@ -2,7 +2,6 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ElementsModule } from './elements/elements.module';
 import { ViewerComponent } from './viewer.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 describe('ViewerComponent', () => {
   let component: ViewerComponent;
@@ -10,7 +9,7 @@ describe('ViewerComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [FlexLayoutModule, ElementsModule, RouterTestingModule],
+      imports: [ElementsModule, RouterTestingModule],
       declarations: [ViewerComponent],
     }).compileComponents();
   }));
