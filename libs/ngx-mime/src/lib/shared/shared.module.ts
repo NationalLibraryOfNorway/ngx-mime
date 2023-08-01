79c013f37a3f18f135aae14bf4b434dc78df86ea
Ronny Mikalsen <ronny.mikalsen@nb.no>
2023-08-01 10:43:12 +0200

    refactor removed FlexLayout

--- libs/ngx-mime/src/lib/shared/shared.module.ts
+++ libs/ngx-mime/src/lib/shared/shared.module.ts
@@ -1,16 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MimeMaterialModule } from './mime-material.module';
 
 @NgModule({
-  exports: [
-    CommonModule,
-    FlexLayoutModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MimeMaterialModule,
-  ],
+  exports: [CommonModule, FormsModule, ReactiveFormsModule, MimeMaterialModule],
 })
 export class SharedModule {}
