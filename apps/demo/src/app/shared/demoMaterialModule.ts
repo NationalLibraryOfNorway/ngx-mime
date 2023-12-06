f94cd153ccf0ed15738f42eaa0a35bb55fcbc102
Ronny Mikalsen <ronny.mikalsen@nb.no>
2023-12-06 10:19:55 +0100

    Feat/dm sans

--- apps/demo/src/app/shared/demoMaterialModule.ts
+++ apps/demo/src/app/shared/demoMaterialModule.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatRadioModule } from '@angular/material/radio';
@@ -13,6 +15,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 @NgModule({
   exports: [
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
