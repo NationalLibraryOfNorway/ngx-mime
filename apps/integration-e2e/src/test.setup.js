383936213d32009ee81c769340f2ba0ab4370d2c
Raymond Karstensen <raymond.karstensen@nb.no>
2024-01-22 13:35:09 +0100

    fix: update tsconfigs

--- apps/integration-e2e/src/test.setup.js
+++ apps/integration-e2e/src/test.setup.js
@@ -3,4 +3,10 @@
 require('ts-node').register({
   extends: '../tsconfig.json',
   transpileOnly: true,
+  compilerOptions: {
+    outDir: '../../dist/out-tsc',
+    module: 'commonjs',
+    target: 'es2022',
+    types: ['node', '@axe-core/playwright', '@playwright/test'],
+  },
 });
