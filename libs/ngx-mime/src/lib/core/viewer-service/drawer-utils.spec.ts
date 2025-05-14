1906a2f0eef587e21e592df3fa2afce89e60201f
Ole-Morten <ole.trones@nb.no>
2025-05-14 13:26:47 +0200

    fix: rename to match actual function

--- libs/ngx-mime/src/lib/core/viewer-service/drawer-utils.spec.ts
+++ libs/ngx-mime/src/lib/core/viewer-service/drawer-utils.spec.ts
@@ -9,34 +9,34 @@ describe('getCanvasDrawType', () => {
   it('should return html for iOS device (userAgent)', () => {
     mockIOS();
 
-    expectCanvasDrawTypeToBe(CanvasRenderer.HTML);
+    expectCanvasRenderTypeToBe(CanvasRenderer.HTML);
   });
 
   it('should return html for iOS via MacIntel + touch', () => {
     mockMacTouch();
 
-    expectCanvasDrawTypeToBe(CanvasRenderer.HTML);
+    expectCanvasRenderTypeToBe(CanvasRenderer.HTML);
   });
 
   it('should return canvas for macOS desktop (no touch)', () => {
     mockMacDesktop();
 
-    expectCanvasDrawTypeToBe(CanvasRenderer.CANVAS);
+    expectCanvasRenderTypeToBe(CanvasRenderer.CANVAS);
   });
 
   it('should return webgl for Linux platform', () => {
     mockLinux();
 
-    expectCanvasDrawTypeToBe(CanvasRenderer.WEBGL);
+    expectCanvasRenderTypeToBe(CanvasRenderer.WEBGL);
   });
 
   it('should return webgl for Windows', () => {
     mockWindows();
 
-    expectCanvasDrawTypeToBe(CanvasRenderer.WEBGL);
+    expectCanvasRenderTypeToBe(CanvasRenderer.WEBGL);
   });
 });
 
-function expectCanvasDrawTypeToBe(platform: string) {
+function expectCanvasRenderTypeToBe(platform: string) {
   expect(getCanvasRenderType()).toBe(platform);
 }
