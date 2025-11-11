f7630c421f1d9abdec002638ff22e0025f1c9bf9
Raymond Karstensen <raymond.karstensen@nb.no>
2025-11-11 11:29:54 +0100

    fix: remove modules and services from list of providers
    Modules should be imported where they are needed.
    Should not be necessary to provide injectable services

--- apps/integration/src/main.ts
+++ apps/integration/src/main.ts
@@ -4,16 +4,9 @@ import {
 } from '@angular/cdk/overlay';
 import { provideHttpClient } from '@angular/common/http';
 import { enableProdMode } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import {
-  bootstrapApplication,
-  BrowserModule,
-  HammerModule,
-} from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, Routes } from '@angular/router';
-import { MimeModule } from '@nationallibraryofnorway/ngx-mime';
 import { AppComponent } from './app/app.component';
 import { ViewerComponent } from './app/viewer/viewer.component';
 import { environment } from './environments/environment';
@@ -49,11 +42,6 @@ bootstrapApplication(AppComponent, {
     provideNoopAnimations(),
     provideHttpClient(),
     provideRouter(appRoutes),
-    BrowserModule,
-    HammerModule,
-    MatMenuModule,
-    MatButtonModule,
-    MimeModule,
     { provide: OverlayContainer, useClass: FullscreenOverlayContainer },
   ],
 }).catch((err) => console.log(err));
