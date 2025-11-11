33bc8fa5a096096fc618f1bb085472043678f73d
Raymond Karstensen <raymond.karstensen@nb.no>
2025-11-11 08:51:08 +0100

    fix: remove modules and services from list of providers
    Modules should be imported where they are needed.
    Should not be necessary to provide injectable services

--- apps/demo/src/main.ts
+++ apps/demo/src/main.ts
@@ -4,12 +4,7 @@ import {
 } from '@angular/cdk/overlay';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { enableProdMode } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import {
-  bootstrapApplication,
-  BrowserModule,
-  HammerModule,
-} from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
   PreloadAllModules,
@@ -19,9 +14,6 @@ import {
 } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { AuthInterceptor } from './app/core/auth.interceptor';
-import { ManifestService } from './app/core/manifest-service/manifest.service';
-import { StyleManagerService } from './app/core/navbar/theme-picker/style-manager/style-manager.service';
-import { ThemeService } from './app/core/navbar/theme-picker/theme-service/theme.service';
 import { ViewerComponent } from './app/viewer/viewer.component';
 import { environment } from './environments/environment';
 
@@ -39,12 +31,6 @@ bootstrapApplication(AppComponent, {
     provideAnimations(),
     provideHttpClient(),
     provideRouter(appRoutes, withPreloading(PreloadAllModules)),
-    BrowserModule,
-    HammerModule,
-    MatSidenavModule,
-    ManifestService,
-    ThemeService,
-    StyleManagerService,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: OverlayContainer, useClass: FullscreenOverlayContainer },
   ],
