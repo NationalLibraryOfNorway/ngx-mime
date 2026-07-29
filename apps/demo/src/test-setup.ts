a24612d972a69b3c4f29688d5da04cc8567349c8
Ronny Mikalsen <ronny.mikalsen@nb.no>
2026-07-29 11:42:08 +0000

    feat(tests): refactor test setup to use setupZoneTestEnv for improved error handling

--- apps/demo/src/test-setup.ts
+++ apps/demo/src/test-setup.ts
@@ -1,9 +1,6 @@
-import 'jest-preset-angular/setup-jest';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
-// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
-globalThis.ngJest = {
-  testEnvironmentOptions: {
-    errorOnUnknownElements: true,
-    errorOnUnknownProperties: true,
-  },
-};
+setupZoneTestEnv({
+  errorOnUnknownElements: true,
+  errorOnUnknownProperties: true,
+});
